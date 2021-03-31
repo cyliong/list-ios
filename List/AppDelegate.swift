6ee0f10542a930362f94bb7fe40ba5e7dcdddc3e
cyliong <liongtheprogrammer@gmail.com>
2021-03-31 22:01:20 +0800

    Handle database migration with Realm.Configuration

--- List/AppDelegate.swift
+++ List/AppDelegate.swift
@@ -1,11 +1,18 @@
 import UIKit
+import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        let config = Realm.Configuration(schemaVersion: 1)
+        Realm.Configuration.defaultConfiguration = config
+        
         return true
     }
 
